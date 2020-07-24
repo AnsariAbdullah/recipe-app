b4c81d669bd93be5f3d6d3230c384ec4cd73e13a
abdullah-desynova <abdullah@desynova.com>
2020-07-24 14:51:05 +0530

    make api call and get data using useEffect

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ import './App.css';
 const App = () => {
 	const APP_ID = '3c6696ef';
 	const APP_KEY = 'edc3892a05b4c235648d3f34cd64f41a';
-	const expample_API = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	useEffect(()=>{
+		getRecipe();
+	}, [])
+
+	const getRecipe = async () => {
+		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+		const data = await response.json();
+		console.log('data ===> ', data)
+
+		// API using promises
+		/* fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+		.then(response => {
+			response.json()
+		}) */
+	}
 
 	return(
 		<div>
 			<form className="search-form">
 				<input className="search-bar" type="text"/>
-				<button className="search-button" type="submit">Search</button>
+				<button className="search-button" type="submit">
+					Search
+				</button>
 			</form>
 		</div>
 	)
