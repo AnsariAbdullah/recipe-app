81d050e5633a8dacaa52f585f6406f267d2d3c44
abdullah-desynova <abdullah@desynova.com>
2020-07-24 09:59:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
+
+const App = () => {
+	const APP_ID = '3c6696ef';
+	const APP_KEY = 'edc3892a05b4c235648d3f34cd64f41a';
+	const expample_API = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	return(
+		<div>
+			<form className="search-form">
+				<input className="search-bar" type="text"/>
+				<button className="search-button" type="submit">Search</button>
+			</form>
+		</div>
+	)
 }
 
 export default App;
