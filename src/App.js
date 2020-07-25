337db1c1a6af92f0411e6610c7270ed8618c81b0
abdullah-desynova <abdullah@desynova.com>
2020-07-25 13:26:58 +0530

    add filter

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ const App = () => {
 
 	const [recipes, setRecipes] = useState([]);
 	const [search, setSearch] = useState("");
-	const [query, setQuery] = useState("banana");
+	const [query, setQuery] = useState("");
 	const [page, setPage] = useState(0);
+	const [filter, setFilter] = useState("");
 
 	useEffect(()=>{
 		getRecipe();
 		// console.log('useEffect run')
-	}, [query, page])
+	}, [query, page, filter])
 
 	const getRecipe = async () => {
 		// API using promises
@@ -28,7 +29,7 @@ const App = () => {
 		}) */
 
 		// API using async-await
-		const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${page}`)
+		const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${page}${filter}`);
 		const data = await response.json();
 		setRecipes(data.hits);
 		console.log(data.hits);
@@ -63,9 +64,68 @@ const App = () => {
 		setSearch('');
 	}
 
+	const Indian = () => {
+		setFilter(filter + '&cuisineType=indian');
+	}
+
+	const Chinese = () => {
+		setFilter(filter + '&cuisineType=chinese');
+	}
+
+	const French = () => {
+		setFilter(filter + '&cuisineType=french');
+	}
+
+	const Breakfast = () => {
+		setFilter(filter + '&mealType=breakfast');
+	}
+
+	const Lunch = () => {
+		setFilter(filter + '&mealType=lunch');
+	}
+
+	const Dinner = () => {
+		setFilter(filter + '&mealType=dinner');
+	}
+
+	const Snack = () => {
+		setFilter(filter + '&mealType=snack');
+	}
+
+	const AlcoholFree = () => {
+		setFilter(filter + '&health=alcohol-free');
+	}	
+
+	const ClearFilter = () => {
+		setFilter('');
+	}
+
+
 	return(
 		<div className="app">
 			<Header />
+			{
+				query && 
+				<div className="filter">
+					<p className="filter-title">Cuisine Type</p>
+					<div className="checkbox-wrapper">
+						<span onClick={Indian} className="checkbox">Indian</span>
+						<span onClick={Chinese} className="checkbox">Chinese</span>
+						<span onClick={French} className="checkbox">French</span>
+					</div>
+					<p className="filter-title">Meal Type</p>
+					<div className="checkbox-wrapper">
+						<span onClick={Breakfast} className="checkbox">Breakfast</span>
+						<span onClick={Lunch} className="checkbox">Lunch</span>
+						<span onClick={Dinner} className="checkbox">Dinner</span>
+						<span onClick={Snack} className="checkbox">Snack</span>
+					</div>
+
+					<span onClick={AlcoholFree} className="checkbox">Alcohol free</span>
+
+					<span onClick={ClearFilter} className="checkbox">Clear</span>
+				</div>
+			}
 			<form onSubmit={getSearch} className="search-form">
 				<input
 					className="search-bar"
@@ -78,18 +138,22 @@ const App = () => {
 					Search
 				</button>
 			</form>
-			<div className="recipes">
-				{recipes.map((recipe, index)=>(
-					<Recipe
-						key={index}
-						title={recipe.recipe.label}
-						calories={recipe.recipe.calories}
-						image={recipe.recipe.image}
-						ingredients={recipe.recipe.ingredients}
-						healthLabels={recipe.recipe.healthLabels}
-					/>
-				))}
-			</div>
+			{
+				recipes ? 	
+				<div className="recipes">
+					{recipes.map((recipe, index)=>(
+						<Recipe
+							key={index}
+							title={recipe.recipe.label}
+							calories={recipe.recipe.calories}
+							image={recipe.recipe.image}
+							ingredients={recipe.recipe.ingredients}
+							healthLabels={recipe.recipe.healthLabels}
+						/>
+					))}
+				</div>
+				: <h3>Nothing in the selected category</h3>
+			}
 			{
 				query ? 
 				<div className="page">
